00ab1ae82e8eca68bba317e2cb4ef6dcdd1642db
hasanej <eljabirhasan@gmail.com>
2024-08-19 12:11:53 +0700

    Add Autowired annotation example

--- src/main/java/hsn/spring/core/services/UserService.java
+++ src/main/java/hsn/spring/core/services/UserService.java
@@ -2,6 +2,7 @@ package hsn.spring.core.services;
 
 import hsn.spring.core.repository.UserRepository;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,7 +10,13 @@ public class UserService {
     @Getter
     private UserRepository userRepository;
 
+    @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public UserService(UserRepository userRepository, String param) {
+        this.userRepository = userRepository;
+        String name = param;
     }
 }
